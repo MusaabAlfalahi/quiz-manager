35e78975a9b2d2baa928dbb7b5ae937f59add452
MusaabAlfalahi <mosaab.dulimi@yahoo.com>
2024-01-30 21:43:19 +0300

    update quiz.service

--- src/modules/quiz/services/quiz.service.ts
+++ src/modules/quiz/services/quiz.service.ts
@@ -10,18 +10,15 @@ export class QuizService {
   constructor(@InjectRepository(Quiz) private quizRepository: Repository<Quiz>) {}
 
   async getAllQuiz(): Promise<Quiz[]> {
-    return await this.quizRepository
-      .createQueryBuilder("q")
-      .leftJoinAndSelect("q.questions", "qt")
-      .leftJoinAndSelect("qt.options", "o")
-      .getMany();
+    return await this.quizRepository.find({ relations: { questions: { options: true } } });
+    // * another way: .createQueryBuilder("q").leftJoinAndSelect("q.questions", "qt").leftJoinAndSelect("qt.options", "o").getMany();
   }
 
   async getQuizById(id: number): Promise<Quiz> {
     return await this.quizRepository.findOne({
       where: { id },
-      // relations: ["questions", "questions.options"],
       relations: { questions: { options: true } },
+      // * another way: relations: ["questions", "questions.options"],
     });
   }
 
